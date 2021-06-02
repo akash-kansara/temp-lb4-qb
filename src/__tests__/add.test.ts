9838f12ed16b13026a4024b0c8ca61819e907b2b
Akash Kansara <akash.kansara@sourcefuse.com>
2021-06-02 10:21:12 +0530

    test(core): added test

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -6,4 +6,8 @@ describe('add tests', () => {
     const result = add(1, 2);
     expect(result).to.eql(3);
   });
+  it('adds 1 +ve & 1 -ve numbers', () => {
+    const result = add(1, -1);
+    expect(result).to.eql(0);
+  });
 });
